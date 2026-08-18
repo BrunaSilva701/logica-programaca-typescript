e9d7346ae1cf0c2166aca60050e68990f34faa05
Bruna <brunasilva.sms@gmail.com>
2026-08-17 21:25:01 -0300

    novas linhas antes da correcao

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@
 // Criar uma fnção que aceita dois argumentos do tipo numerico
 // Exibe a soma entre os dois
 
-function soma (a: number, b:number){
+function Soma (a: number, b:number){
     this.a = a;
     this.b = b;
 }
+
+let soma = new Soma(a+b);
