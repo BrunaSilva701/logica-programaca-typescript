f2062618dc9386efc25a953a30047d3026c80ae6
Bruna <brunasilva.sms@gmail.com>
2026-08-17 21:22:32 -0300

    codigo

---
+++ index.ts
@@ -0,0 +1,8 @@
+// projeto para compilar e executar codigo
+// Criar uma fnção que aceita dois argumentos do tipo numerico
+// Exibe a soma entre os dois
+
+function soma (a: number, b:number){
+    this.a = a;
+    this.b = b;
+}
